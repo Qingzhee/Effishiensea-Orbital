703ba1126bf765b468f99907558706af3e4ecaaa
aldentantan <aldentanfk@gmail.com>
2024-07-28 16:43:40 +0800

    Fix auth warning when app starts

--- app/Firebase/FirebaseConfig.ts
+++ app/Firebase/FirebaseConfig.ts
@@ -1,7 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+//Please let us know if the app doesn't work because of this import
+//We imported this to remove a warning but now there's a red line but the app works fine for me?
+//Lmk if we missed something and your app authentication is messed up
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth"; 
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB_O6cDAVjccdTMSO8KpJZBn858YK1Clkg",
@@ -15,6 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP); 
